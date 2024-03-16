998e483d4163616c9acb7a4b0bb3f5417f0d254c
Carlos Henrique Trindade <119907560+carloshenrique44@users.noreply.github.com>
2024-03-15 21:45:12 -0300

    Add files via upload

---
+++ Atividade03.java
@@ -0,0 +1,11 @@
+public class Atividade03 {
+    public static void main (String[] args) {
+        String nome = "Carlos";
+        String cpf = "13422845956";
+        String Cep = "84560253";
+        String Cidade = "Curitiba"; 
+        String Estado = "Parana";
+
+        System.out.println(" A pessoa com o nome " + nome + " resgistrada no cpf " + cpf + " é residente no cep " + Cep + " na cidade " + Cidade + " " + Estado);
+    }
+}
