8ba903a3981a3443f72d9269021e76c8b20471b4
Carlos Henrique Trindade <119907560+carloshenrique44@users.noreply.github.com>
2024-04-04 00:02:30 -0300

    Add files via upload

---
+++ aula1string.java
@@ -0,0 +1,21 @@
+public class aula1string {
+    public static void main (String[] args) {
+        String nome = "Carlos Henrique Trindade";
+        String primeironome = nome.substring(0, 6);
+
+        System.out.println(primeironome);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
