bae597abed3e8a8379f0335ae9c406095c0c1340
Carlos Henrique Trindade <119907560+carloshenrique44@users.noreply.github.com>
2024-04-04 00:05:10 -0300

    Add files via upload

---
+++ aula09desafio2.java
@@ -0,0 +1,9 @@
+
+public class aula09desafio2 {
+    public static void main(String[] args) {
+        String nome = "Carlos Henrique Trindade";
+        String primeironome = nome.toUpperCase().substring(0, 6);
+
+        System.out.println(primeironome);
+    }
+}
